368861bb32d92cc044e0ed04215fd8cbf64d24cc
kaamara <67202966+kaamara@users.noreply.github.com>
2026-03-02 02:57:19 +0100

    Update internet-stats.sh

--- internet-stats.sh
+++ internet-stats.sh
@@ -3,7 +3,7 @@ echo "        NETWORK STATS        "
 echo "-----------------------------"
 
 echo "==================="
-echo "--- IP ADDRESS  ---"
+echo "----IP ADDRESS-----"
 echo "==================="
 ip=$(hostname -I | awk '{print $1}')
 interface=$(ip route | awk '/default/ { print $5 }')
@@ -11,13 +11,13 @@ echo "IP Address: $ip"
 echo "Interface: $interface"
 
 echo "==================="
-echo "--- GATEWAY     ---"
+echo "-----GATEWAY------"
 echo "==================="
 gateway=$(ip route | awk '/default/ {print $3}')
 echo "Gateway: $gateway"
 
 echo "==================="
-echo "--- DNS SERVERS ---"
+echo "----DNS SERVERS----"
 echo "==================="
 dns=$(grep "nameserver" /etc/resolv.conf | awk '{print $2}'| xargs)
 echo "DNS Servers: $dns"
